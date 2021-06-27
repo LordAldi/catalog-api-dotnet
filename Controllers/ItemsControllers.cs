1e609c41a824e84d3b9fb95f83c481c566b6ae78
LordAldi <aldianugra09@gmail.com>
2021-06-28 02:19:20 +0700

    POST, PUT, DELETE route

--- Controllers/ItemsControllers.cs
+++ Controllers/ItemsControllers.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.Controllers
 {
-    [Route("[controller]")]
+    [Route("items")]
     [ApiController]
     public class ItemsController : ControllerBase
     {
@@ -32,6 +32,50 @@ namespace Catalog.Controllers
                 return NotFound();
             }
             return item.AsDto();
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+            repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
+        }
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+            repository.UpdateItem(updatedItem);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+            repository.DeleteItem(id);
+            return NoContent();
         }
 
     }
